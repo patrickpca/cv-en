0be28e6ed92ea6a12183f155ff1ee1098f060a6a
patrickpca <81128104+patrickpca@users.noreply.github.com>
2024-05-06 07:01:06 -0300

    Update cv

--- src/metadata/metadata.js
+++ src/metadata/metadata.js
@@ -1,6 +1,6 @@
 module.exports = {
   name: 'Patrick Coser',
-  title: 'CyberSecurity || Pentester || AppSec',
+  title: 'Data Processor',
   facts: {
     'Local': '<a href="https://www.google.com/maps/d/embed?mid=1njMqPYaeTpLPTg1FGom0BoJJA3o&hl=en&ehbc=2E312F" width="640" height="480"><i class="fa fa-home fact-icon"></i>Espirito Santo</a>',
     'Linkedin': `<a href="https://www.linkedin.com/in/patrick-coser/">
@@ -10,23 +10,42 @@ module.exports = {
     'Email': '<a href="patrickpcaa@gmail.com"><i class="fa fa-envelope fact-icon"></i>patrickpcaa@gmail.com</a>'
   },
   skills: [
-    ['Hacking', 100],
-    ['OSINT', 100],
-    ['Pentest', 100],
-    ['BACEN', 85],
-    ['Data Analysis', 75],
-    ['COBIT', 75],
-    ['SQL', 80],
-    ['GitHub', 75],
-    ['OpenIA', 80],
-    ['Cloud', 75],
-    ['Python', 80],
-    ['Vulnerability analysis', 100],
-  ],
+    ['Team Work', 100],
+    ['Flexibity', 100],
+    ['Data Analysis', 100],
+    ['Linux', 100],
+    ['Python', 100],
+    ['Qimera', 90],
+    ['FMGeocoder', 85],
+    ['ArcGis', 85],
+    ['QGis', 85],
+    ['EIVA', 80],
+    ['Hypack', 75],
+    ['GlobalMapper', 75],
+    ['AutoCad', 60],
+    ['CARIS', 60],
+    ],
   positions: [
+       
+    {
+      title: 'Freelance - DataProcessor',
+      period: 'October 2023 – Now',
+      skills: ['Qimera', 'AutoClean', 'Eiva', 'QGis', 'FMGeocoder', 'Python', 'GlobalMapper'],
+      contents: `
+      - Qimera Operation for Bathymetric Data: Efficient use of Qimera software for processing and analyzing depth 
+      data
+      - Optimization with AutoClean and qimera: Application of automatic filters to enhance the quality of 
+      bathymetric data
+      - Data Management with EIVA: Strategies for effective management of large volumes of bathymetric data using 
+      EIVA
+      - Multisource Data Integration: Combining bathymetric data with other geospatial data sources for 
+      comprehensive analysis
+`
+    },
+    
     {
       title: 'Mazars',
-      period: 'November de 2022 – Now',
+      period: 'November 2022 – Now',
       skills: ['Pentest', 'Audit', 'Vulnerability Assessment'],
       contents: `
       - Internal and External Pentesting
@@ -35,35 +54,25 @@ module.exports = {
       - Monitoring, auditing, and assessing the maturity of information security processes
       - Analysis and mitigation of risks and vulnerabilities in cyber security systems, applications, processes, 
       and infrastructures.
+      - Automate internal processes, creating interactive dashboards and web systems
 `
     },
-    {
-      title: 'Pentester Freelancer',
-      period: 'February de 2022 – Now',
-      skills: ['Pentest ', 'Vulnerability Assessment'],
-      contents: `
-- External Pentest
-- Internal Pentest
-- Web Pentest
-- security analysis
-`
-    },
-    
+
     {
       title: 'EGS Brasil',
-      period: 'April de 2018 – July de 2021',
+      period: 'April 2018 – July 2021',
       skills: ['Data Analysis', 'Python', 'Geoprocessing'],
       contents: `
-      Data processing using Python and elaboration of technical reports for clients, with automation methods
-      implemented using       the language for document creation. Libraries utilized include Numpy, Pandas, 
-      Matplotlib, Cartopy, Seaborn, gsw, pylab, and pycircstat. Maintenance, testing, and development of electronic 
-      equipment procedures tailored to the field of oceanography. Acquisition of data through watercraft, conducting 
-      data surveys, and installation of tide gauges.
+      Data processing with Python and the development of technical reports for clients, incorporating 
+      automated methods for document creation using the language. Utilized libraries include Numpy, Pandas, 
+      Matplotlib, Cartopy, Seaborn, gsw, pylab, and pycircstat. Maintenance, testing, and development of 
+      procedures for oceanographic electronic equipment such as SBES, ADCP, CTD and HOBO. Data acquisition 
+      via watercraft, conducting surveys, and installing tide gauges.
 `
    },
    {
     title: 'Ecoceano - Junior Consulting Company in Oceanography and Environmental Education',
-    period: 'February de 2016 – December de 2016',
+    period: 'February 2016 – December 2016',
     skills: ['Environmental Education', 'Cash Control', ' Accounting'],
     contents: `
     Managing cash flow, preparing informative reports, conducting strategic planning, and supervising advisors in the 
@@ -73,53 +82,101 @@ module.exports = {
 
 
   positions2: [
-    {
-      title: 'Enygma',
-      period: 'April de 2022 – June de 2022',
-      skills: ['SOC', 'Fortinet'],
-      contents: `
-      Fortinet:
-      Log analysis;
-      Risk identification;
-      Vulnerability report analysis.
-`
-    },
+    
     
    {
-    title: 'Na Onda do Futuro (Riding the Wave of the Future)',
-    period: 'March de 2016 – December de 2016',
-    skills: ['Environmental Education'],
+    title: 'Data Processor',
+    period: 'December 2016 - Now',
+    skills: ['Anaconda','Github','Python','PHP','Matlab','ArcGis','Qgis','Autocad', 'Hypack', 'Qimera','GlobalMapper','FMGeocoder'],
     contents: `
-    Development of classroom activities focused on environmental education for children.`
-  }
+    Data analysis; Data qualification; Creation of data processing workflows; Automation of data processing.`
+  },
+  {
+    title: 'Cyber Security',
+    period: 'January 2021 - Now',
+    skills: ['Linux', 'Python','SQL','C','Ruby','Power Shell','Bash','Nmap', 'Nessus','Shodan','GitLab','GitHub','AWS','Azure'],
+    contents: `
+    Internal and External Pentesting; Web application pentest; Vulnerability analysis.`
+  },
+  
   ],
 
-
-
-
+  // PROJETOS REALIZADOS
+  // PROJETOS REALIZADOS
+  // PROJETOS REALIZADOS
+  // PROJETOS REALIZADOS
 
   experience: [
     {
-      header: 'Pentest',
-      skills: ['Kali Linux', 'Burp Suite','Metasploit','PowerSploit','Inveigh','Owasp Amass','Dehashed','Nmap', 'Nessus','Breach-Parsed','Shodan','GitLab','GitHub','AWS','Azure','Customização de scripts em Bash, PHP, SQL, Python, C, Perl, Ruby, PowerShell'],
+      header: 'EGS UK - MBES Data Processor -  Mar 2023 - Apri 2023)',
+      skills: ['Qimera','AutoClean','FMGT','QGis','GlobalMapper','Python','Excel'],
       contents: `
-      Internal and External Pentesting; Web application pentest; Vulnerability analysis.
+      - Bathymetric Data Processing in Sunderland, UK: Efficient handling of depth data collected off the coast of 
+      Sunderland. 
+      - Data Acquisition with SSS, SBP, and Multibeam: Strategies for capturing underwater data using Side Scan Sonar (SSS), 
+      Sub-bottom Profiling (SBP), and Multibeam systems.
+      - Daily Deliverables Export for QC: Routine export of daily acquired lines for quality control assessments.
+      `
+    },
+        
+    {
+      header: 'Shore Approach - Petrobras (Jul 2019 - Mar 2021)',
+      skills: ['Github','Python','PHP','Matlab','ArcGis','Qgis','Autocad', 'Hypack', 'HOBOware','Global Mapper', 'Edgetech Discover',
+      'SignatureWaves', 'Echart', 'Ruskin', 'SeatermV2'],
+      contents: `
+      - Coastal monitoring Jaconé (RJ), Cabiúnas (RJ), Barra do Riacho (ES) e Regência (ES). 
+      - Monthly campaigns with data acquisition of beach profiles with SBES and RTK, Quality control, and technical report.
+      - Tide gauge installation. Mooring and retrieval of ADCP and CTD with deployment in a Sonardyne LRT. 
+      - Sidescan sonar data acquisition.
       `
     },
     {
-      header: 'Auditor/Consultant',
-      skills: ['COBIT','National Institute of Standards (NIST)','International Organization for Standardization (ISO 27K)','Information Security Forum (ISF)','Standard of Good Practice for Information Security (SOGP)','Committee of Sponsoring Organizations of the Treadway Commission (COSO II)'],
+      header: 'Programa Amazônia Integrada Sustentável (PAIS) - Rede Nacional de Ensino e Pesquisa (RNP) (May - jun 2021)',
+      skills: ['Python','SeatermV2','Ruskin','ITER SYSTEMS BathySwath echosounder','SBG SYSTEMS Ellipse 2 MRU','Teledyne DMS-05 MRU'],
       contents: `
-      Implementation of security policies, risk analysis and mitigation, and flow mapping.
+      - Conducting a pre-laying cable route survey along the Amazon River, this project incorporates bathymetric and sonographic mapping, 
+      seismic data acquisition, and the collection of environmental, sedimentological, and topographical data. It utilizes Bathyswath
+      - Processor and Global Mapper software for comprehensive analysis and mapping.
       `
     },
     {
-      header: 'Data Science',
-      skills: ['Anaconda','Github','Python','PHP','Matlab','ArcGis','Qgis','Autocad'],
+      header: 'Pre cable lay route survey (Oct 2021)',
+      skills: ['MBES R2Sonic2024 single head','Valeport CTD','Python'],
       contents: `
-      Data analysis; Data qualification; Creation of data processing workflows; Automation of data processing.
+      - The project, undertaken for the client Ellalink, is executed with the advanced CARIS HIPS and SIPS software, focusing
+      on onshore activities in Fortaleza, Brazil. This project utilizes the state-of-the-art MBES R2Sonic2024 single head 
+      instrument to ensure precise mapping and surveying outcomes.
       `
-    }
+    },
+    {
+      header: 'Centrais Eletricas de Sergipe (Nov 2020 - Mar 2021)',
+      skills: ['BathSwatch Echosounder','Valeport CTD','SBG System MRU', 'Teledyne DMS-05 MRU'],
+      contents: `
+      - The Coast Morphodynamics Monitoring Program, commissioned by CELSE – Centrais Elétricas de Sergipe S.A., utilizes advanced software
+      tools like Bathyswath Processor and QGIS for onshore operations in Sergipe, Brazil. The project employs cutting-edge instruments, 
+      including the ITER SYSTEMS BathySwath echosounder, SWiFT Valeport CTD, SBG SYSTEMS Ellipse 2 MRU, and Teledyne DMS-05 MRU, to 
+      ensure precise and efficient monitoring of coastal dynamics.
+      `
+    },
+    {
+      header: 'Alcatel Submarine Network - ASN (Apr - jun 2019)',
+      skills: ['Hypack','Echart','Scanline','HOBOware','Geosuite Acquisiton','Edgetech Discover','Sonardyne Ranger','Valeport Datalog','Sonarwiz','Qinsy','Qimera'],
+      contents: `
+      - Cable route survey in Rio de Janeiro and Santos - Brasil. Mobilization of vessels, side scan sonar, SBES and RTK, data acquisition, 
+      and quality control.
+      `
+    },
+    {
+      header: 'Shore Approach - Petrobras (Out - Dec 2018)',
+      skills: ['Hypack','Echart','Scanline','HOBOware','Global Mapper', 'Edgetech Discover','Nortek Aquadopp','SIG500'],
+      contents: `
+      - Coastline surveillance in Guamaré - Brasil involves monthly operations utilizing Single Beam Echo Sounders (SBES) and 
+      Real-Time Kinematic (RTK) systems for beach profile data collection and quality control. This includes the installation of tide 
+      gauges, as well as the deployment and retrieval of Acoustic Doppler Current Profilers (ADCP). Additionally, data is acquired using 
+      side-scan sonar technology.
+      `
+    },
+   
   ]
 
 };
